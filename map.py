3d83007c8365a596595924338419bb7f1b794b1f
Raja Palla <palla.raja@gmail.com>
2017-01-03 01:10:39 -0500

    updated map.py to route requests for "place" requested, by rendering view mappy.html.
    We enable the route in such a way that, it gets lat, lng details from business and renders mappy.html fulfilling it with dictionary of lat, lng.

--- map.py
+++ map.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
+from business import getCoordinates
 
 # encapsulating this program in Flask framework server before running it
 app = Flask(__name__)
+
+# Api to show 4 types of maps with mapIds as ROADMAP, TERRAIN, HYBRID, SATELLITE
+@app.route('/<place>')
+def getMap(place):
+    dictMap = getCoordinates(place)
+    return render_template("mappy.html", locator=dictMap, place=place)
 
 # Application Home page
 @app.route('/')
